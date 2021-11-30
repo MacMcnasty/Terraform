6c93868b178c9a6435673526d28bed9adb288999
MacMcnasty <mcnasty72@gmail.com>
2021-11-30 14:44:11 -0500

    Added user_data for httpd server & page

--- main.tf
+++ main.tf
@@ -4,6 +4,13 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-04902260ca3d33422"
     instance_type = "t3.micro"
+    user_data = <<EOF
+                #!/bin/bash
+                yum install httpd -y
+                echo "<html><body><h1>Hello Everyone</h1></body></html>" >/var/www/html/index.html
+                systemctl start httpd
+                systemctl enable httpd
+                EOF
 
     tags = {
         Name = "Terraform-Example"
