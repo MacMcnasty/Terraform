807833039fcb222ed8740b3cde9d9e1efed1d2bd
MacMcnasty <mcnasty72@gmail.com>
2021-11-30 16:18:59 -0500

    Single Server done

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-04902260ca3d33422"
     instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
     user_data = <<EOF
                 #!/bin/bash
                 yum install httpd -y
@@ -15,4 +16,20 @@ resource "aws_instance" "example" {
     tags = {
         Name = "Terraform-Example"
     }
+}
+
+resource "aws_security_group" "instance"{
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port 
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type = number
+    default = 80
 }
